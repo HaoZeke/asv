bca8d57f9a0acb0edfc3a8c98fde524eb8b525d5
Rohit Goswami <rgoswami@ieee.org>
2026-04-20 04:23:46 +0200

    MAINT: fix profile tests on Python 3.15

--- test/tools.py
+++ test/tools.py
@@ -590,7 +590,6 @@ def _build_dummy_wheels(tmpdir, wheel_dir, to_build, build_conda=False):
             f.write(f"__version__ = '{version}'")
 
         subprocess.check_call([sys.executable, '-mpip', 'wheel',
-                               '--build-option=--universal',
                                '-w', wheel_dir,
                                '.'],
                               cwd=build_dir)
